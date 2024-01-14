cd7ebf86b1692a4be92f6c785dacde95c9ec3257
vamosgs <gegham.samvelyan@hotmail.com>
2024-01-14 14:11:34 +0400

    Update metadata in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,10 @@ import { Providers } from './providers';
 
 export const metadata = {
   title: 'FTN DOMAINS',
-  description: 'Name Service for FTN',
+  description:
+    'Bahamut Name Service. Decentralised naming for Asset Wallets & more',
+  keywords:
+    'ftn, ens, bns, bahmut name service, fastext, opengate, ftn domains',
 };
 
 export default function RootLayout({ children }) {
@@ -10,22 +13,6 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <head>
         <link rel='icon' href='/favicon.ico' sizes='any' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta charSet='utf-8' />
-        <meta name='description' content='Bahamut Name Service.' />
-        <meta
-          name='keywords'
-          content='ftn, ens, bns, bahmut name service, fastext, opengate, ftn domains'
-        />
-        <meta name='author' content='FTN' />
-        <meta name='title' content='FTN.DOMAINS' />
-        <meta name='og:title' property='og:title' content='FTN.DOMAINS' />
-        <meta
-          name='og:description'
-          property='og:description'
-          content='Bahamut Name Service.'
-        />
-        <meta name='og:image' property='og:image' content='/og-image.png' />
       </head>
       <body>
         <Providers>
